e7717e2d99d872c11da883f2f75fd665ac7cface
RB12-pixel <beluricky92@gmail.com>
2026-08-10 18:59:07 +0200

    Add IcypalMod class for mod initialization

---
+++ src/main/Java/com/icypal/IcypalMod.java
@@ -0,0 +1,8 @@
+package com.icypal;
+import net.fabricmc.api.ModInitializer;
+public class IcypalMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Icypal Mod loaded for 26.2!");
+    }
+}
